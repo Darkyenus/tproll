1c28d289f8d0500a1097d5e6a7e50d193faf3b43
Darkyenus <darkyenus@gmail.com>
2019-02-27 16:31:18 +0100

    Docs
    Took 9 minutes

--- src/main/java/com/darkyen/tproll/logfunctions/ILogFileHandler.java
+++ src/main/java/com/darkyen/tproll/logfunctions/ILogFileHandler.java
@@ -1,15 +1,24 @@
 package com.darkyen.tproll.logfunctions;
 
+import org.slf4j.Marker;
+
 /**
- * Interface for file handlers.
- * Log file handlers are an abstraction over ideal log sink, and handle file selection, writing, closing, etc.
+ * Actual file writing inside {@link FileLogFunction} is delegated to implementations of this interface.
+ * This is an abstraction over ideal log sink, the implementation must handle file selection, writing, closing, etc.,
+ * though it is not limited only to files.
+ *
+ * Methods may be called from arbitrary thread, but always mutually exclusively.
  */
 public interface ILogFileHandler {
 
+    /** Called before first {@link #log(CharSequence)} invocation */
     void initialize();
 
+    /** Called by {@link FileLogFunction#log(String, long, byte, Marker, CharSequence)} with the message which should
+     * get logged. */
     void log(CharSequence message);
 
+    /** Called by {@link FileLogFunction#dispose()}. */
     void dispose();
 
 }
